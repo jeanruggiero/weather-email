6c8b6f1f1bc26a01876bdeef9807413cf80700ac
Jean Ruggiero <jeanruggiero@gmail.com>
2019-06-19 19:26:45 -0700

    Created basic newsletter signup screen and subscriber model. Fixes#1.

--- weatherapp/newsletter/models.py
+++ weatherapp/newsletter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Subscriber(models.Model):
+    email = models.EmailField()
+    #location = models.CharField(max_length=128)
+
+
+# class Location(models.Model):
+#     name = models.CharField(max_length=128)
+
--- weatherapp/newsletter/views.py
+++ weatherapp/newsletter/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from .models import Subscriber
 
-# Create your views here.
+
+def newsletter_signup(request):
+    if request.method == 'GET':
+        return render(request, 'newsletter_signup.html')
+    if request.method == 'POST':
+        subscriber = Subscriber(
+            email=request.POST['email'],
+        )
+        subscriber.save()
+        return HttpResponseRedirect(reverse(newsletter_signup))
+
+

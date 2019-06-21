bdb126177b19d10159d0438a062699d3feb31b6e
Jean Ruggiero <jeanruggiero@gmail.com>
2019-06-20 23:01:26 -0700

    Refactoring sendmail to improve testability.

--- weatherapp/newsletter/management/commands/sendmail.py
+++ weatherapp/newsletter/management/commands/sendmail.py
@@ -8,86 +8,136 @@ import statistics
 from ...models import Subscriber
 
 
-class WeatherReport():
-
-    def __init__(self, temperature, precip, summary):
-        self.temperature = temperature
-        self.precip = precip
-        self.summary = summary
-
-
-def current_weather_request(**kwargs):
+class CurrentWeatherGetter:
     """
-    Returns the current weather queried using the params specified in **kwargs. The weather is
-    returned as a WeatherReport object with SI units.
+    Encapsulates code to get the current weather.
     """
-    base_url = 'https://api.weatherbit.io/v2.0/'
+    base_url = 'https://api.weatherbit.io/v2.0/current?'
 
-    params = kwargs
-    params['units'] = 'I'
-    params['key'] = os.environ.get('WEATHER_API_KEY')
+    def __init__(self, location):
+        self.location = location
 
-    query_string = 'current?'
-    query_string += '&'.join(key + '=' + str(value) for key, value in params.items())
-    response = requests.get(base_url + query_string)
+    @property
+    def params(self):
+        """
+        Returns a dict containing the parameters required for the HTTP GET request
+        for current weather.
+        """
+        return {'units': 'I',
+                'lat': self.location.latitude,
+                'lon': self.locatiton.longitude,
+                'key': os.environ.get('WEATHER_API_KEY')}
 
-    data = json.loads(response.content.decode())['data'][0]
-    return WeatherReport(data['temp'], data['precip'], data['weather']['description'])
+    @property
+    def query_string(self):
+        """
+        Returns a query string for the HTTP GET request.
+        """
+        return '&'.join(key + '=' + str(value) for key, value in self.params.items())
+
+    def request_weather(self):
+        """
+        Performs an HTTP GET request using the base_url and query_string.
+        """
+        response = requests.get(self.base_url + self.query_string)
+        return json.loads(response.content.decode())['data'][0]
+
+    @property
+    def temperature(self):
+        """Returns current temperature in deg F."""
+        return self.request_weather()['temp']
+
+    @property
+    def precip(self):
+        """Returns current precipitation type."""
+        return self.request_weather()['precip']
+
+    @property
+    def summary(self):
+        """Returns a summary of the weather."""
+        return self.request_weather()['weather']['description'])
 
 
-def get_historical_weather(location):
+class HistoricalTemperatureGetter:
     """
-    Returns the average daily temperature in deg F for the previous 7 days.
+    Encapsulates code to get the current weather.
     """
 
     base_url = 'http://api.apixu.com/v1/'
 
-    params = dict()
-    params['key'] = os.environ.get('HISTORICAL_API_KEY')
-    params['q'] = '{},{}'.format(location.latitude, location.longitude)
+    def __init__(self, location):
+        self.location = location
+        self.date = datetime.date.today()
 
-    year = datetime.datetime.now().year
-    current_day = datetime.datetime.now().day
-    month = datetime.datetime.now().month
-    temperatures = []
+    def params(self, date):
+        """
+        Returns a dict containing the parameters required for the HTTP GET request
+        for historical weather. date arg is a python date object.
+        """
 
-    for day in range(current_day-7, current_day-1):
-        params['dt'] = '{:%Y-%m-%d}'.format(datetime.date(year, month, day))
+        return {'key': os.environ.get('HISTORICAL_API_KEY'),
+                'q': '{},{}'.format(self.location.latitude, self.location.longitude),
+                'dt': '{:%Y-%m-%d}'.format(date)}
 
-        query_string = 'history.json?'
-        query_string += '&'.join(key + '=' + str(value) for key, value in params.items())
+    @staticmethod
+    def query_string(params):
+        """
+        Returns a query string for the HTTP GET request.
+        """
+        return '&'.join(key + '=' + str(value) for key, value in params.items())
 
-        response = requests.get(base_url + query_string)
-        temperatures.append(json.loads(response.content.decode())['forecast']['forecastday'][0]['day']['avgtemp_f'])
-
-    return statistics.mean(temperatures)
-
-
-def get_current_weather(location):
-    """
-    Returns the weather for a given location and optional time. If time argument
-    is not provided, the current weather will be returned.
-    """
-    return current_weather_request(lat=location.latitude, lon=location.longitude)
+    def request_temperature(self, date):
+        response = requests.get(self.base_url + self.query_string(self.params(date)))
+        return json.loads(response.content.decode())['forecast']['forecastday'][0]['day']['avgtemp_f']
 
 
-def email_subject(current_weather, historical_temperature):
-    """
-    Returns an email subject based on how the current weather at the recipient's location
-    compares with the average weather at that location.
-    """
 
-    if current_weather.summary in ['Clear sky', 'Few clouds', 'Scattered clouds'] or \
-            historical_temperature - current_weather.temperature >= 5:
-        return "It's nice out! Enjoy a discount on us."
-    elif current_weather.precip or historical_temperature - current_weather.temperature <= -5:
-        return "Not so nice out? That's okay, enjoy a discount on us."
-    else:
-        return "Enjoy a discount on us."
+    @property
+    def typical_temperature(self):
+        # need to figure out range of dates
+        return statistics.mean(self.request_temperature(date) for date in range())
 
 
-def email_body(weather, location):
-    return f"The weather in {location} is currently {weather.temperature} deg F and {weather.summary}."
+class EmailSender:
+
+    def __init__(self, current_weather_getter, historical_weather_getter):
+
+        self.current_weather_getter = current_weather_getter
+        self.historical_weather_getter = historical_weather_getter
+
+        self.subscribers = Subscriber.objects.order_by('location')
+
+        # Use a set comprehension for locations such that only one set of API calls is made per unique location
+        self.locations = {subscriber.location for subscriber in subscribers}
+        self.current_weather = {location: CurrentWeatherGetter(location) for location in locations}
+        self.historical_temperature = {location: get_historical_temperature(location) for location in locations}
+
+    def email_subject(current_weather, historical_temperature):
+        """
+        Returns an email subject based on how the current weather at the recipient's location
+        compares with the average weather at that location.
+        """
+
+        if current_weather.summary in ['Clear sky', 'Few clouds', 'Scattered clouds'] or \
+                historical_temperature - current_weather.temperature >= 5:
+            return "It's nice out! Enjoy a discount on us."
+        elif current_weather.precip or historical_temperature - current_weather.temperature <= -5:
+            return "Not so nice out? That's okay, enjoy a discount on us."
+        else:
+            return "Enjoy a discount on us."
+
+    @property
+    def body(self):
+        return f"The weather in {location} is currently {weather.temperature} deg F and {weather.summary}."
+
+    @property
+    def subject(self):
+        retturn email_subject(current_weather[subscriber.location],
+                                historical_temperature[subscriber.location])
+
+    def send_mail_to_all(self):
+        for subscriber in self.subscribers:
+            send_mail(self.subject, self.body, 'jeanruggiero@gmail.com', [subscriber.email])
 
 
 class Command(BaseCommand):
@@ -95,20 +145,22 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **kwargs):
-        subscribers = Subscriber.objects.order_by('location')
-        locations = {subscriber.location for subscriber in subscribers}
-        current_weather = {location: get_current_weather(location) for location in locations}
-        historical_weather = {location: get_historical_weather(location) for location in locations}
+        # subscribers = Subscriber.objects.order_by('location')
+        # locations = {subscriber.location for subscriber in subscribers}
+        # current_weather = {location: get_current_weather(location) for location in locations}
+        # historical_temperature = {location: get_historical_temperature(location) for location in locations}
 
-        for subscriber in subscribers:
+        # for subscriber in subscribers:
+        #
+        #     # subject = email_subject(current_weather[subscriber.location],
+        #     #                         historical_temperature[subscriber.location])
+        #     #
+        #     # body = email_body(current_weather[subscriber.location], subscriber.location)
+        #
+        #     send_mail(subject,
+        #               body,
+        #               'jeanruggiero@gmail.com',
+        #               [subscriber.email])
 
-            subject = email_subject(current_weather[subscriber.location],
-                                    historical_weather[subscriber.location])
-
-            body = email_body(current_weather[subscriber.location], subscriber.location)
-
-            send_mail(subject,
-                      body,
-                      'jeanruggiero@gmail.com',
-                      [subscriber.email])
+        EmailSender().send_mail_to_all()
 
--- weatherapp/newsletter/tests.py
+++ weatherapp/newsletter/tests.py
@@ -1,3 +1,95 @@
 from django.test import TestCase
+from .models import Subscriber, Location
+from unittest.mock import MagicMock
 
-# Create your tests here.
+from .management.commands.sendmail import *
+
+
+class SubscriberTestCase(TestCase):
+    fixtures = ['test_fixture.json']
+    def setUp(self):
+        pass
+
+
+class CurrentWeatherTests(TestCase):
+    def setUp(self):
+        loc = Location(lat=2, lon=5)
+
+    def test_get_current_weather(self):
+
+        self.current_weather_request = MagicMock()
+        get_curent_weather(loc)
+
+        self.current_weather_request.assert_called_with(lat=)
+
+
+        def current_weather_request(**kwargs):
+            """
+            Returns the current weather queried using the params specified in **kwargs. The weather is
+            returned as a WeatherReport object with SI units.
+            """
+            base_url = 'https://api.weatherbit.io/v2.0/'
+
+            params = kwargs
+            params['units'] = 'I'
+            params['key'] = os.environ.get('WEATHER_API_KEY')
+
+            query_string = 'current?'
+            query_string += '&'.join(key + '=' + str(value) for key, value in params.items())
+            response = requests.get(base_url + query_string)
+
+            data = json.loads(response.content.decode())['data'][0]
+            return WeatherReport(data['temp'], data['precip'], data['weather']['description'])
+
+        def get_historical_temperature(location):
+            """
+            Returns the average daily temperature in deg F for the previous 7 days.
+            """
+
+            base_url = 'http://api.apixu.com/v1/'
+
+            params = dict()
+            params['key'] = os.environ.get('HISTORICAL_API_KEY')
+            params['q'] = '{},{}'.format(location.latitude, location.longitude)
+
+            year = datetime.datetime.now().year
+            current_day = datetime.datetime.now().day
+            month = datetime.datetime.now().month
+            temperatures = []
+
+            for day in range(current_day - 7, current_day - 1):
+                params['dt'] = '{:%Y-%m-%d}'.format(datetime.date(year, month, day))
+
+                query_string = 'history.json?'
+                query_string += '&'.join(key + '=' + str(value) for key, value in params.items())
+
+                response = requests.get(base_url + query_string)
+                temperatures.append(
+                    json.loads(response.content.decode())['forecast']['forecastday'][0]['day']['avgtemp_f'])
+
+            return statistics.mean(temperatures)
+
+        def get_current_weather(location):
+            """
+            Returns the weather for a given location and optional time. If time argument
+            is not provided, the current weather will be returned.
+            """
+            return current_weather_request(lat=location.latitude, lon=location.longitude)
+
+        def email_subject(current_weather, historical_temperature):
+            """
+            Returns an email subject based on how the current weather at the recipient's location
+            compares with the average weather at that location.
+            """
+
+            if current_weather.summary in ['Clear sky', 'Few clouds', 'Scattered clouds'] or \
+                    historical_temperature - current_weather.temperature >= 5:
+                return "It's nice out! Enjoy a discount on us."
+            elif current_weather.precip or historical_temperature - current_weather.temperature <= -5:
+                return "Not so nice out? That's okay, enjoy a discount on us."
+            else:
+                return "Enjoy a discount on us."
+
+        def email_body(weather, location):
+            return f"The weather in {location} is currently {weather.temperature} deg F and {weather.summary}."
+
